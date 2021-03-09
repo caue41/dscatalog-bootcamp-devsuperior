8e9a4dc21345eee4993d63b1c3d07b8b3de095d6
Cauê Gonçalves Santana <caue41@gmail.com>
2021-03-09 20:26:58 -0300

    Product details route

---
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const ProductDetails = () => {
+    return (
+        <div className="product-details-container">
+            <h1>Product details</h1>
+        </div>
+    );
+};
+
+export default ProductDetails;
