7a638395839ef45d7199e9d081798163093a0076
Cauê Gonçalves Santana <caue41@gmail.com>
2021-03-15 15:08:14 -0300

    Pagination

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,25 +5,27 @@ import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
+import Pagination from 'core/components/Pagination';
 
 const Catalogo = () => {
 
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
 
         setIsLoading(true);
-        makeRequest ({url: '/products', params})
+        makeRequest({ url: '/products', params })
             .then(response => setProductsResponse(response.data))
             .finally(() => {
                 setIsLoading(false);
             })
-    }, []);
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -34,11 +36,18 @@ const Catalogo = () => {
                 {isLoading ? <ProductCardLoader /> : (
                     productsResponse?.content.map(product => (
                         <Link to={`/products/${product.id}`} key={product.id}>
-                            <ProductCard product={product}/>
+                            <ProductCard product={product} />
                         </Link>
                     ))
                 )}
             </div>
+            {productsResponse && (
+                <Pagination
+                    totalPages={productsResponse?.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />
+            )}
         </div>
     );
 }
