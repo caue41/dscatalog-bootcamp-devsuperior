76dba7929e3c292345dc47c7a7aa547302668144
Cauê Gonçalves Santana <caue41@gmail.com>
2021-07-20 20:32:33 -0300

    Add allowed roles on private route

--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 export const CLIENT_ID = 'dscatalog';
 export const CLIENT_SECRET = 'dscatalog123';
 
@@ -10,6 +11,14 @@ type LoginResponse = {
     userId: number;
 }
 
+export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type AcessToken = {
+    exp: number;
+    user_name: string;
+    authorities: Role[];
+}
+
 export const saveSessionData = (loginResponse: LoginResponse) => {
     localStorage.setItem('authData', JSON.stringify(loginResponse));
 }
@@ -17,5 +26,33 @@ export const saveSessionData = (loginResponse: LoginResponse) => {
 export const getSessionData = () => {
     const sessionData = localStorage.getItem('authData') || '{}';
     const parsedSessionData = JSON.parse(sessionData);
+
     return parsedSessionData as LoginResponse;
+}
+
+export const getAcessTokenDecoded = () => {
+    const sessionData = getSessionData();
+
+    const tokenDecoded = jwtDecode(sessionData.access_token);
+    return tokenDecoded as AcessToken;
+}
+
+export const isTokenValid = () => {
+    const { exp } = getAcessTokenDecoded();
+    return Date.now() <= exp*1000;
+}
+
+export const isAuthenticated = () => {
+    const sessionData = getSessionData();
+    return sessionData.access_token && isTokenValid();
+}
+
+export const isAllowedByRole = (routeRoles: Role[] = []) => {
+    if (routeRoles.length === 0) {
+        return true;
+    }
+
+    const { authorities } = getAcessTokenDecoded();
+
+    return routeRoles.some(role => authorities.includes(role));
 }
